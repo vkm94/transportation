66c734053242654d66623f33ade963ec9d07bfc8
varun <varunmanila@gmail.com>
2024-04-18 00:35:52 +0530

    First commit

---
+++ TransportApp/src/main/java/com/example/TransportApp/TransportAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.TransportApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportAppApplication.class, args);
+	}
+
+}
---
+++ TransportApp/src/test/java/com/example/TransportApp/TransportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TransportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
