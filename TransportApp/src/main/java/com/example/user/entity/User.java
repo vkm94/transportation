c6029a877f2eb183582492bb31ba20b25f22d2c3
varun <varunmanila@gmail.com>
2024-04-18 01:11:06 +0530

    added db config

--- TransportApp/src/main/java/com/example/user/entity/User.java
+++ TransportApp/src/main/java/com/example/user/entity/User.java
@@ -10,12 +10,19 @@ public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name")
     private String firsNname;
+    @Column(name = "flast_name")
     private String lastName;
+    @Column(name = "user_name")
     private String  userName;
+    @Column(name = "password")
     private String password;
+    @Column(name = "email")
     private String email;
+    @Column(name = "phone_number")
     private  String phoneNumber;
+    @Column(name = "address")
     private String address;
 
 
